3814f9d51dba4ad97ab4861e2024d60308f0bd77
Zoe Millard <zmillard@stevens.edu>
2018-12-06 23:13:18 -0500

    adding leftover files

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../familyTree/react-client/dist`));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/../familyTree/react-client/dist/index.html`));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}!`);
+});
+
+
