e7ce58ee7cfdc58332edb57ff2eaf0a1e1c1d2fa
Zoe Millard <zmillard@stevens.edu>
2018-12-06 22:57:03 -0500

    first commit

---
+++ react-client/src/Components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+        </div>
+      </Router>
+    );
+  };
+};
+export default App;
