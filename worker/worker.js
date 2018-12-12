28ed6ca01e35ae921bc34bb8934bdfa73d1adc37
Max Remetz <max@remetz.com>
2018-12-12 14:49:33 -0500

    Worker connects to mongo

--- worker/worker.js
+++ worker/worker.js
@@ -1,4 +1,12 @@
 const redisConnection = require("./redis-connection");
+const MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/famDB";
+
+MongoClient.connect(url, function(err, db) {
+    if(err) throw err;
+    console.log("Database Connection Established!");
+    db.close();
+});
 
 function submitEvent(event, reqId, data, name) {
     if (data == "loading") {
