41e0d20d13c009ff7be321e41adabed3760a293e
Akin, Ayse N <aakin@stevens.edu>
2018-12-12 15:25:46 -0500

    Basic outline of dashboard complete.

--- react-client/src/Components/Dashboard.jsx
+++ react-client/src/Components/Dashboard.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import {
-    Navbar
-} from 'reactstrap';
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <nav>
+                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                     <a className="navbar-brand" href="#">
                         <p>Create</p>
                         <p>View</p>
                     </a>
                 </nav>
                 <main role="main">
+                    <div className="jumbotron">
+                        <div className="container">
+                            <h1 className="display-3"> Welcome to Your Family Tree</h1>
+                            <p> INSERT TREE COMPONENT HERE.</p>
+                        </div>
+                    </div>
                 </main>
             </div>
         );
