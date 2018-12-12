d83b52e5ac64ac1bd9b0b1fd952974b315305451
Akin, Ayse N <aakin@stevens.edu>
2018-12-12 16:58:36 -0500

    Added ability to use props and created login and nav components

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Dashboard from "./Dashboard";
+import Nav from "./Nav";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Nav />
           <Dashboard />
         </div>
       </Router>
--- react-client/src/Components/Dashboard.jsx
+++ react-client/src/Components/Dashboard.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
+import Login from "./Login";
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-                    <a className="navbar-brand" href="#">
-                        <p>Create</p>
-                        <p>View</p>
-                    </a>
-                </nav>
+                
                 <main role="main">
                     <div className="jumbotron">
                         <div className="container">
                             <h1 className="display-3"> Welcome to Your Family Tree</h1>
-                            <p> INSERT TREE COMPONENT HERE.</p>
+                            <Login />
+                            {/* <p> INSERT TREE COMPONENT HERE.</p> */}
                         </div>
                     </div>
                 </main>
