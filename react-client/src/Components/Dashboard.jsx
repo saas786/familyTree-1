223fce7907fde0e1f5db9639e4f43b464e63bec3
Akin, Ayse N <aakin@stevens.edu>
2018-12-12 23:49:31 -0500

    Created login and signup modals

--- react-client/src/Components/Dashboard.jsx
+++ react-client/src/Components/Dashboard.jsx
@@ -10,8 +10,6 @@ class Dashboard extends Component {
                     <div className="jumbotron">
                         <div className="container">
                             <h1 className="display-3"> Welcome to Your Family Tree</h1>
-                            <Login />
-                            {/* <p> INSERT TREE COMPONENT HERE.</p> */}
                         </div>
                     </div>
                 </main>
