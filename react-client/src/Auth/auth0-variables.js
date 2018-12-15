d36da577bec11cd2aaec5945732ff1190cb9ece7
Akin, Ayse N <aakin@stevens.edu>
2018-12-15 12:49:19 -0500

    Added comments for locations where there were issues with auth0

--- react-client/src/Auth/auth0-variables.js
+++ react-client/src/Auth/auth0-variables.js
@@ -2,4 +2,5 @@ export const AUTH_CONFIG = {
   domain: 'cs554fp.auth0.com',
   clientId: 'xV4a_dZPrgHtTn103G5v371MjrvUujUm',
   callbackUrl: 'http://localhost:3000/'
+  //callbackUrl should be: 'http://localhost:3000/view but cannot be because of the way routing was done.
 }
