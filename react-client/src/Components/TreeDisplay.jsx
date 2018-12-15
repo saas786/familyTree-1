c1fffae0dd322836fc11dec327766eae7eddc53b
Akin, Ayse N <aakin@stevens.edu>
2018-12-15 02:18:16 -0500

    Started with KeyCloak

--- react-client/src/Components/TreeDisplay.jsx
+++ react-client/src/Components/TreeDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Tree from "react-d3-tree";
 import PersonNode from './PersonNode';
+import Keycloak from 'keycloak-js';
 
 /* Uses https://github.com/bkrem/react-d3-tree for basic tree logic*/
 
@@ -63,35 +64,54 @@ const myTreeData = [
 ];
 
 class TreeDisplay extends React.PureComponent {
-  state = {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      keycloak: null,
+      authenticated: false,
+      x: 0,
+      y: 0
+    };
+  }
 
   componentDidMount() {
+    const keycloak = Keycloak('/keycloak.json');
+    keycloak.init({onLoad: 'login-require'}).then(authenticated => {
+      this.setState({keycloak: keycloak, authenticated: authenticated })
+    })
+
     const dimensions = this.treeContainer.getBoundingClientRect();
     this.setState({
       translate: {
         x: dimensions.width / 2,
         y: dimensions.height / 4
       }
-    });
+    })
   };
 
   render() {
-    return (
-      <div id="treeWrapper" ref={tc => (this.treeContainer = tc)}>
+    if (this.state.keycloak) {
+      if (this.state.authenticated) return (
+        <div id="treeWrapper" ref={tc => (this.treeContainer = tc)}>
         <Tree
         data={myTreeData}
         pathFunc="elbow"
-        orientation="verticle"
+        orientation="vertical"
         translate={this.state.translate}
         allowForeignObjects
         nodeLabelComponent={{
           render: <PersonNode className='myLabelComponentInSvg'/>,
           foreignObjectWrapper: {
-            y: 24,
+            y: 15,
             x: 5
           }
         }}/>
       </div>
+      ); else return(
+        <div> Unable to authenticate! Please try again. </div>)
+    }
+    return (
+      <div> Initializing Keycloak... </div>
     );
   }
 }
