a46029e8c9ab63456c950ade36de02b5371ed06e
Zoe Millard <zmillard@stevens.edu>
2018-12-15 12:19:16 -0500

    fixes email stuff

--- react-client/src/Components/Dashboard.jsx
+++ react-client/src/Components/Dashboard.jsx
@@ -3,6 +3,20 @@ import Login from "./Login";
 import Signup from "./Signup";
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: ""
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(event) {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
@@ -11,7 +25,9 @@ class Dashboard extends Component {
                         <div className="container">
                             <h1 className="display-3"> Welcome to Your Family Tree</h1>
                             <p>Our application allows you to trace your patrilineal or matrilineal line (mothers side or fathers side) and explore your heritage.</p>
-                             <p>Please login in order to begin your journey and don't forget to tell your family to join<a id="sendEmail" href="mailto:zmillard@stevens.edu?subject=Learn%20Your%20Family%20Tree&amp;body=Come%20join%20me%20at%20localhost:3000%20and%20make%20your%20family%20tree!"> here!</a></p>
+                            <p>Please login in order to begin your journey and don't forget to tell your family to join
+                            <br/>
+                            <label>Reccomend a friend! Enter email: <input type='text' onChange={this.handleChange} value={this.state.email}></input></label><a id="sendEmail" href={`mailto:${this.state.email}?subject=Learn%20Your%20Family%20Tree&amp;body=Come%20join%20me%20at%20localhost:3000%20and%20make%20your%20family%20tree!`}> and then click here!</a></p>
                         </div>
                     </div>
                     <ul class="list-inline text-center">
