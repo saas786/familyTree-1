303b7b6e217530dedb00c82585b30dbc2b560f87
Akin, Ayse N <aakin@stevens.edu>
2018-12-14 19:25:26 -0500

    Fixed variable states

--- react-client/src/Components/Signup.jsx
+++ react-client/src/Components/Signup.jsx
@@ -4,6 +4,10 @@ class Signup extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            firstname: "",
+            lastname: "",
+            age: null,
+            gender: "",
             username: "",
             password: ""
         };
@@ -35,25 +39,26 @@ class Signup extends Component {
                                 <form>
                                     <div class="form-group">
                                         <label for="firstname"> First Name: </label>
-                                        <input type="text" class="form-control" id="firstname" value={this.state.first_name} onChange={this.handleChange}></input>
-
+                                        <input type="text" class="form-control" id="firstname" placeholder="Enter your first name" value={this.state.firstname} onChange={this.handleChange}></input>
                                         <label for="lastname"> Last Name: </label>
-                                        <input type="text" class="form-control" id="lastname" value={this.state.last_name} onChange={this.handleChange}></input>
-
+                                        <input type="text" class="form-control" id="lastname" placeholder="Enter your last name" value={this.state.lastname} onChange={this.handleChange}></input>
                                         <label for="age"> Age: </label>
                                         <input type="number" class="form-control" id="age" value={this.state.age} onChange={this.handleChange}></input>
+                                        
                                         <div class="radio">
                                             <label for="gender"> Gender: </label> <br />
-                                            <input type="radio" id="gender" value={this.state.gender} onChange={this.handleChange} /> Male <br />
-                                            <input type="radio" id="gender" value={this.state.gender} onChange={this.handleChange} /> Female <br />
-                                            <input type="radio" id="gender" value={this.state.gender} onChange={this.handleChange} />Prefer not to say
+                                            <input type="radio" id="gender" value="Male" checked={this.state.gender==="Male"} onChange={this.handleChange} /> Male <br />
+                                            <input type="radio" id="gender" value="Female" checked={this.state.gender==="Female"} onChange={this.handleChange} /> Female <br />
+                                            <input type="radio" id="gender" value="Other" checked={this.state.gender==="Other"} onChange={this.handleChange} />Prefer not to say
                                         </div>
+
                                         <label for="username">Create a Username</label>
-                                        <input type="text" class="form-control" id="username" placeholder="Enter your username" value={this.state.user_name} onChange={this.handleChange}></input>
+                                        <input type="text" class="form-control" id="username" placeholder="Enter your username" value={this.state.username} onChange={this.handleChange}></input>
                                         <label for="password">Create a Password</label>
                                         <input type="password" class="form-control" id="password" placeholder="Enter your password" data-toggle="password" value={this.state.password} onChange={this.handleChange}></input>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Submit</button>
+                                    <button type="reset" class="btn btn-secondary">Reset</button>
                                 </form>
                             </div>
                         </div>
