ec5446a4df8af00fe826bbdf6aba729871605ba5
Zoe Millard <zmillard@stevens.edu>
2018-12-14 22:28:20 -0500

    fixes size of container

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const Home = () => < Dashboard />;
+const Create = () => <h2>Create</h2>;
+const View = () => <TreeDisplay />;
 
 class App extends Component {
   render() {
---
+++ react-client/src/Components/TreeDisplay.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import Tree from "react-d3-tree";
+import PersonNode from './PersonNode';
+
+/* Uses https://github.com/bkrem/react-d3-tree for basic tree logic*/
+
+const myTreeData = [
+  {
+    name: "Jay Pritchett",
+    attributes: {
+      spouse: "Gloria Pritchett",
+    },
+    children: [
+      {
+        name: "Claire Pritchett",
+        attributes: {
+          spouse: "Phil Dunphy"
+        },
+        children: [
+          {
+            name: "Hayley Dunphy",
+            attributes: {
+              spouse: ""
+            }
+          },{
+            name: "Alex Dunphy",
+            attributes: {
+              spouse: ""
+            }
+          },{
+            name: "Luke Dunphy",
+            attributes: {
+              spouse: ""
+            }
+          }
+        ]
+      },{
+        name: "Mitchell Pritchett",
+        attributes: {
+          spouse: "Cameron Tucker"
+        },
+        children: [
+          {
+            name: "Lily Pritchett-Tucker",
+            attributes: {
+              spouse: ""
+            }
+          }
+        ]
+      },{
+        name: "Joe Pritchett",
+        attributes: {
+          spouse: ""
+        }
+      },{
+        name: "Manny Delgado",
+        attributes: {
+          spouse: ""
+        }
+      }
+    ]
+  }
+];
+
+class TreeDisplay extends Component {
+  render() {
+    return (
+      //{/* <Tree /> will fill width/height of its container; in this case `#treeWrapper` */}
+      <div id="treeWrapper">
+        <Tree data={myTreeData} pathFunc="elbow" orientation="verticle" allowForeignObjects 
+        nodeLabelComponent={{
+          render: <PersonNode className='myLabelComponentInSvg'/>,
+          foreignObjectWrapper: {
+            y: 24,
+            x: 5
+          }
+        }
+      }/>
+      </div>
+    );
+  }
+}
+export default TreeDisplay;
